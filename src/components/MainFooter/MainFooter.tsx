ab0525d678bb3ed841bf831dfa93c13fef8d9973
negative318 <dotiendung1234567891@gmail.com>
2024-03-12 23:40:53 +0700

    ....

--- src/components/MainFooter/MainFooter.tsx
+++ src/components/MainFooter/MainFooter.tsx
@@ -3,39 +3,51 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function MainFooter() {
   return (
-    <div>
-
-      <div className='container gap-5 flex'>
-          <div className='w-1/2 h-full'>
-            <div className='text-3xl text-darkText font-semibold'>Liên Hệ</div>
-            <img src='' alt=''></img>
+    <div className='mt-10 border-none bg-blue-100'>
+      <div className='pt-10 pb-10 container gap-5 flex text-darkText'>
+        <div className='w-7/12 h-full'>
+            <div className='pl-28 pb-10 text-3xl font-semibold'>Liên Hệ</div>
+            <div className="flex items-center justify-center">
+              <img className="w-4/6" src="https://i.ibb.co/DtMw32j/location.jpg" alt="" />
+            </div>
           </div>
-          <div className='w-1/2 h-full inline-block'>
+          <div className='pt-20 w-5/12 h-full inline-block '>
             <div className='flex'>
-              <div>
+              <div className='items-center text-2xl px-4 rounded-full bg-darkText p-3 font-bold text-lightText'>
                 <FontAwesomeIcon icon={faEnvelope} />
               </div>
-              <div className='inline-block'>
-                <div className=''>Công tác - dịch vụ</div>
-                <div className=''>defuniversity@defuni.edu.vn</div>
+              <div className='inline-block pl-4'>
+                <div className='font-semibold text-xl text-darkText py-1'>Công tác - dịch vụ</div>
+                <div className='text-darkText text-sm'>defuniversity@defuni.edu.vn</div>
               </div>
             </div>
-            <div className='flex'>
-              <div>
+            <div className='flex pt-12'>
+              <div className='items-center text-2xl px-4 rounded-full bg-darkText p-3 font-bold text-lightText'>
                 <FontAwesomeIcon icon={faEnvelope} />
               </div>
-              <div className='inline-block'>
-                <div className=''>Công tác - dịch vụ</div>
-                <div className=''>defuniversity@defuni.edu.vn</div>
+              <div className='inline-block pl-4'>
+                <div className='font-semibold text-xl py-1'>Quản lý sinh viên</div>
+                <div className='text-darkText'>quanlysinhvien@defuni.edu.vn</div>
               </div>
             </div>
-            <div className='flex'>
-              <div>
+            <div className='flex pt-12'>
+              <div className='items-center text-2xl px-4 rounded-full bg-darkText p-3 font-bold text-lightText'>
                 <FontAwesomeIcon icon={faPhone} />
               </div>
-              <div className='inline-block'>
-                <div className=''>Công tác - dịch vụ</div>
-                <div className=''>defuniversity@defuni.edu.vn</div>
+              <div className='inline-block pl-4'>
+                <div className='font-semibold text-xl py-1'>Số điện thoại dịch vụ</div>
+                <div className='text-darkText'>0868712144</div>
+              </div>
+            </div>
+
+
+
+            <div className='py-10 text-darkText'>
+              <div className='font-bold'>WORKING HOURS</div>
+              <div>
+                <div>Thứ hai - Thứ 6: 08:00 -19:30</div>
+                <div>Thứ bảy: 10:00 - 16:30</div>
+                <div>Chủ nhật: 10:00 - 16:30</div>
               </div>
             </div>
           </div>
@@ -45,6 +57,5 @@ export default function MainFooter() {
         <div className='text-xs w-full text-center bg-slate-700 p-2 text-gray-400'>Copyright 2024 © LTNC</div>
       </div>
     </div>
-    
   )
 }
--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -1,73 +1,64 @@
-import { useViewport } from 'src/hooks/useViewport'
-import React, { useState } from 'react'
-import mainPath from 'src/constants/path'
-import {Link, NavLink} from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useContext } from 'react'
+import { NavLink } from 'react-router-dom'
+import mainPath from 'src/constants/path'
+import { AppContext } from 'src/contexts/app.context'
+
 export default function MainHeader() {
-  const viewport = useViewport()
-  const isSmall = viewport.width < 768
+  const { isAuthenticated } = useContext(AppContext)
 
-  //? Style
-  const popoverStyle = 'border border-black/20 rounded-lg min-w-52 py-3 px-2 text-sm dekstop:text-base'
-  const wrapperStyle = 'text-darkText font-medium flex flex-col space-y-1'
-  const itemStyle =
-    'tablet:hover:text-white hover:text-black px-4 tablet:px-3 py-1.5 duration-200 tablet:hover:bg-primaryBlueHovering/80 tablet:rounded-md'
-
-
-    const [menus] = useState ([
-      {
-          name: 'TRANG CHỦ',
-          path: mainPath.home,
-      },
-      {
-          name: 'TÀI KHOẢN',
-          path: mainPath.personal,
-      },
-      {
-          name: 'KHÓA HỌC',
-          path: mainPath.courseList,
-      },
-    ]);
-
-
+  const menus = [
+    {
+      name: 'TRANG CHỦ',
+      path: mainPath.home
+    },
+    {
+      name: 'TÀI KHOẢN',
+      path: mainPath.personal
+    },
+    {
+      name: 'KHÓA HỌC',
+      path: mainPath.courseList
+    }
+  ]
 
   return (
-    <div className='w-full bg-webColor700 flex flex-col h-14 justify-between shrink-0 min-h-full' style={{ minHeight: 'inherit' }}>
-      
-      <div className='container w-full flex text-1xl'>
-
+    <div
+      className='w-full bg-webColor700 flex flex-col h-full justify-between shrink-0 min-h-full'
+      style={{ minHeight: 'inherit' }}
+    >
+      <div className='container flex'>
         <div className='w-1/4 flex justify-start h-14'>
-          <img className='h-full pr-3' src='https://i.ibb.co/T41xQpn/def.png' alt='' />
+          <img
+            src='https://media.istockphoto.com/id/517188688/vi/anh/phong-c%E1%BA%A3nh-n%C3%BAi-non.jpg?s=612x612&w=0&k=20&c=WWWaejSo6EWGZMZSK7QK6LCfwd0rL2KB3ImCX2VkW4A='
+            alt=''
+          />
+          <div className='flex text-center items-center pl-27'>LTNC</div>
+        </div>
 
-
-          <div className='flex text-center items-center ml-27 text-lightText'>
-            LTNC-FE
+        <div className='w-1/2 flex items-center justify-center'>
+          <div className='grid grid-cols-3 gap-x-6 max-w-7x1'>
+            {menus.map((menu, index) => (
+              <NavLink
+                key={index}
+                to={menu.path}
+                className='text-lightText flex space-x-2 py-4 px-6 hover:bg-hoveringBg shrink'
+              >
+                {menu.name}
+              </NavLink>
+            ))}
           </div>
         </div>
 
-
-
-
-
-
-
-
-
-
-        <div className="w-1/2 flex items-center justify-center">
-              <div className="grid grid-cols-3 gap-x-6 max-w-7x1">
-                  {menus.map((menu, index) => (
-                    <NavLink key={index} to={menu.path} className='text-lightText flex space-x-2 py-4 px-6 hover:bg-hoveringBg shrink'>{menu.name}</NavLink>
-                  ))}
-              </div>
-        </div> 
-
-        <div className='w-1/4 flex items-center justify-end'>
-              <NavLink to={mainPath.login} className='text-lightText flex space-x-2 py-4 px-6 hover:bg-hoveringBg shrink'>
+        <div className='w-1/4 flex justify-end'>
+          {!isAuthenticated && (
+            <NavLink to={mainPath.login} className='text-lightText flex space-x-2 py-4 px-6 hover:bg-hoveringBg shrink'>
               <FontAwesomeIcon icon={faUser} />
-                <p className='uppercase font-semibold'>Acount</p>
-              </NavLink>
+              <p className='uppercase font-semibold'>Đăng nhập</p>
+            </NavLink>
+          )}
+          {isAuthenticated && <div className=''>Tài khoản</div>}
         </div>
       </div>
     </div>
