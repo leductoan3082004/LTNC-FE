e779ea0d0b34d2f8bd525ba256310d77c54b5564
tianthecore <thienln2004@gmail.com>
2024-03-18 20:08:23 +0700

    update

--- src/pages/Personal/children/PersonalScore/PersonalScore.tsx
+++ src/pages/Personal/children/PersonalScore/PersonalScore.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
 
 export default function PersonalScore() {
-  return <div>PersonalScore</div>
+  return (
+    <div className='items-center justify-center py-10 bg-webColor200'>
+      <div className='container'>
+        <div className='text-center text-red text-3xl '> BẢNG ĐIỂM SINH VIÊN </div>
+        <div className='flex w-full'>
+          <div className='w-4/12 text-center'> jsdnadj</div>
+        </div>
+      </div>
+    </div>
+  )
 }
