d1ec831ec5d7740e05c7fd631725a98835890d77
tianthecore <thienln2004@gmail.com>
2024-03-20 20:22:18 +0700

    update a  bit sad

--- src/pages/Personal/children/PersonalScore/PersonalScore.tsx
+++ src/pages/Personal/children/PersonalScore/PersonalScore.tsx
@@ -9,18 +9,19 @@ export default function PersonalScore() {
           <ScoreSorting />
         </div>
       </div>
-        <div className='container'>
-          <div className='border h-9 flex items-center py-4 font-bold font-sans text-darkText pl-16 '>Tài Khoản</div>
-          <div className='flex items-center py-3 text-darkText'>
-            <div className='w-1/4 text-right pr-4'>Tên sinh viên: </div>
-            <div className='border rounded-md border-transparent w-3/5 h-10 pt-2.5 px-4'>Dungdo</div>
-          </div>
-          <div className='flex items-center py-3 text-darkText'>
-            <div className='w-1/4 text-right pr-4'>Mã số sinh viên: </div>
-            <div className='border rounded-md border-transparent w-3/5 h-10 pt-2.5 px-4'>2217749</div>
-          </div>
-          <div className='text-center w-4/12'> </div>
+      <div className='container'>
+        <div className='border h-9 border-transparent flex items-center py-4 text-lg desktop:text-xl font-bold text-darkText text-start '>
+          <div className='w-1/4 text-left pr-4'> Tài Khoản </div>
         </div>
+        <div className='border h-9 flex border-transparent items-center py-4 text-lg desktop:text-xl font-bold text-darkText text-start6 '>
+          <div className='w-1/4 text-left pr-4'>Tên sinh viên: </div>
+          <div className='border rounded-md border-transparent w-3/5 h-10 pt-2.5 px-4'>Dungdo</div>
+        </div>
+        <div className='border h-9 border-transparent flex items-center py-4 text-lg desktop:text-xl font-bold text-darkText text-start '>
+          <div className='w-1/4 text-left pr-4'>Mã số sinh viên: </div>
+          <div className='border rounded-md border-transparent w-3/5 h-10 pt-2.5 px-4'>2217749</div>
+        </div>
+      </div>
       <ScoreSortingtByYear year={2024} />
       <ScoreSortingtByYear year={2025} />
       <ScoreSortingtByYear year={2026} />
--- src/pages/Personal/children/PersonalScore/components/ScoreSortingByYear/ScoreSortingByYear.tsx
+++ src/pages/Personal/children/PersonalScore/components/ScoreSortingByYear/ScoreSortingByYear.tsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
-import {personalPath} from 'src/constants/path'
+import { personalPath } from 'src/constants/path'
 import { PersonalscoreContext } from 'src/contexts/personalscore.context' 
 
 interface Props {
@@ -29,39 +29,30 @@ export default function ScoreSortingtByYear({ year }: Props) {
         <div className='border-t-2 border-primaryText w-6/12 desktop:w-4/12'></div>
       </div>
       <div className='overflow-x-auto'>
-        <table className='table-auto w-full text-lg desktop:text-xl uppercase text-darkText text-start'>
+        <table className='table-auto w-full text-lg desktop:text-xl  text-darkText text-start'>
           <thead>
             <tr>
-              <th className="px-4 py-2">ID</th>
-              <th className="px-4 py-2">Name</th>
-              <th className='px-4 py-2'>Age</th>
-              <th className='px-4 py-2'>Email</th>
+              <th className='px-4 py-2 uppercase'>STT</th>
+              <th className='px-4 py-2 uppercase'>Tên môn học</th>
+              <th className='px-4 py-2 uppercase'>Mã môn học</th>
+              <th className='px-4 py-2 uppercase'>Số tín chỉ</th>
+              <th className='px-4 py-2 uppercase'>Điểm</th>
             </tr>
           </thead>
+          <tbody>
+            {Array(5)
+              .fill(0)
+              .map((_, index) => (
+                <tr key={index}>
+                  <td className='border border-black px-4 py-2 text-center text-lg'>{index + 1}</td>
+                  <td className='border border-black px-4 py-2 text-center'>Lập trình nâng cao</td>
+                  <td className='border border-black px-4 py-2 text-center'>CO1010</td>
+                  <td className='border border-black px-4 py-2 text-center'>3</td>
+                  <td className='border border-black px-4 py-2 text-center'>9</td>
+                </tr>
+              ))}
+          </tbody>
         </table>
-      </div>
-      <div className='flex flex-col'>
-        {Array(5)
-          .fill(0)
-          .map((_, index) => (
-            <div
-              className='border-b last:border-none py-4 border-primaryText/80  text-lg desktop:text-xl uppercase text-darkText text-start'
-              key={index}
-            >
-              <div className='overflow-x-auto items-center'>
-                <table className='table-auto w-full '>
-                  <tbody>
-                    <tr>
-                      <td className='border px-4 py-2 items-center'>1</td>
-                      <td className='border px-4 py-2'>John Doe</td>
-                      <td className='border px-4 py-2'>30</td>
-                      <td className='border px-4 py-2'>john@example.com</td>
-                    </tr>
-                  </tbody>
-                </table>
-            </div>
-            </div>
-          ))}
       </div>
     </div>
   )
