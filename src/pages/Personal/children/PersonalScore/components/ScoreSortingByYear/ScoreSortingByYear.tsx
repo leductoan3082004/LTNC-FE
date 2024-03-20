22690412caed06b31418a505548f572dd0174c6c
tianthecore <thienln2004@gmail.com>
2024-03-20 14:32:26 +0700

    update

--- src/pages/Personal/children/PersonalScore/components/ScoreSortingByYear/ScoreSortingByYear.tsx
+++ src/pages/Personal/children/PersonalScore/components/ScoreSortingByYear/ScoreSortingByYear.tsx
@@ -17,12 +17,6 @@ export default function ScoreSortingtByYear({ year }: Props) {
     navigate({ pathname: `${personalPath.score}/${year}` })
   }
 
-  //! HANDLE CHOOSE COURSE
-  const chooseYear = () => () => {
-    setAcademicYear(year.toString())
-    navigate({ pathname: `${personalPath.score}/${year}/$` })
-  }
-
   return (
     <div className='bg-webColor100 rounded-lg py-4 px-6 space-y-4 text-darkText'>
       <button
@@ -34,42 +28,39 @@ export default function ScoreSortingtByYear({ year }: Props) {
       <div className='w-full flex justify-center'>
         <div className='border-t-2 border-primaryText w-6/12 desktop:w-4/12'></div>
       </div>
+      <div className='overflow-x-auto'>
+        <table className='table-auto w-full text-lg desktop:text-xl uppercase text-darkText text-start'>
+          <thead>
+            <tr>
+              <th className="px-4 py-2">ID</th>
+              <th className="px-4 py-2">Name</th>
+              <th className='px-4 py-2'>Age</th>
+              <th className='px-4 py-2'>Email</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
       <div className='flex flex-col'>
         {Array(5)
           .fill(0)
           .map((_, index) => (
-            <button
-              onClick={chooseYear()}
-              className='border-b last:border-none py-4 border-primaryText/80 hover:text-primaryText text-lg desktop:text-xl uppercase text-darkText text-start'
+            <div
+              className='border-b last:border-none py-4 border-primaryText/80  text-lg desktop:text-xl uppercase text-darkText text-start'
               key={index}
             >
-              <div className="overflow-x-auto">
-                <table className='table-auto w-full'>
-                  <thead>
-                    <tr>
-                      <th className="px-4 py-2">ID</th>
-                      <th className="px-4 py-2">Name</th>
-                      <th className="px-4 py-2">Age</th>
-                      <th className="px-4 py-2">Email</th>
-                    </tr>
-                  </thead>
+              <div className='overflow-x-auto items-center'>
+                <table className='table-auto w-full '>
                   <tbody>
                     <tr>
-                      <td className='border px-4 py-2'>1</td>
-                      <td className="border px-4 py-2">John Doe</td>
-                      <td className="border px-4 py-2">30</td>
-                      <td className="border px-4 py-2">john@example.com</td>
-                    </tr>
-                    <tr>
-                      <td className="border px-4 py-2">2</td>
-                      <td className="border px-4 py-2">Jane Doe</td>
-                      <td className="border px-4 py-2">25</td>
-                      <td className="border px-4 py-2">jane@example.com</td>
+                      <td className='border px-4 py-2 items-center'>1</td>
+                      <td className='border px-4 py-2'>John Doe</td>
+                      <td className='border px-4 py-2'>30</td>
+                      <td className='border px-4 py-2'>john@example.com</td>
                     </tr>
                   </tbody>
                 </table>
             </div>
-            </button>
+            </div>
           ))}
       </div>
     </div>
