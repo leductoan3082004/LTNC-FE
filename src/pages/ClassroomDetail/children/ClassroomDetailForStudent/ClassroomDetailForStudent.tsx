f4b97d56a110c5beaac80e455db7679834c8c083
tianthecore <thienln2004@gmail.com>
2024-04-09 23:54:04 +0700

    update

--- src/pages/ClassroomDetail/children/ClassroomDetailForStudent/ClassroomDetailForStudent.tsx
+++ src/pages/ClassroomDetail/children/ClassroomDetailForStudent/ClassroomDetailForStudent.tsx
@@ -1,5 +1,15 @@
-import React from 'react'
+import { useQuery } from '@tanstack/react-query'
+import React, { useContext } from 'react'
+import lessonApi from 'src/apis/lesson.api'
+import { ClassroomContext } from 'src/contexts/classroom.context'
 
 export default function ClassroomDetailForStudent() {
+  const {currentClassroom} = useContext(ClassroomContext)
+
+  //! Get lesson list
+const {}= useQuery({queryKey : ['lesson_list', currentClassroom?.class._id],
+  queryFn: () => lessonApi.listLessons
+} )
+
   return <div>ClassroomDetailForStudent</div>
 }
