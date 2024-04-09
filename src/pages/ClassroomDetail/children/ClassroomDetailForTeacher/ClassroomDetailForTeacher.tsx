d1143fd44efe1f6b511a314e05ff1bc358425727
teddylethal <letienthanh364@gmail.com>
2024-04-09 09:29:36 +0700

    lesson list

--- src/pages/ClassroomDetail/children/ClassroomDetailForTeacher/ClassroomDetailForTeacher.tsx
+++ src/pages/ClassroomDetail/children/ClassroomDetailForTeacher/ClassroomDetailForTeacher.tsx
@@ -2,13 +2,28 @@ import { Fragment, useContext, useState } from 'react'
 import { ClassroomContext } from 'src/contexts/classroom.context'
 import ClassroomTeacherCreateLesson from '../../components/ClassroomTeacherCreateLesson'
 import { useQuery } from '@tanstack/react-query'
+import lessonApi from 'src/apis/lesson.api'
+import { Lesson } from 'src/types/lesson.type'
+import ClassroomLessonCard from '../../components/ClassroomLessonCard'
+
+interface ExtendedLesson extends Lesson {
+  isOpen: boolean
+}
 
 export default function ClassroomDetailForTeacher() {
   const { currentClassroom } = useContext(ClassroomContext)
   const [creatingLesson, setCreatingLesson] = useState<boolean>(false)
 
   //! Get lesson list
-  const {} = useQuery()
+  const { data: lessonListData } = useQuery({
+    queryKey: ['lesson_list'],
+    queryFn: () => lessonApi.listLessons({ classroom_id: currentClassroom?.class._id as string }),
+    enabled: Boolean(currentClassroom)
+  })
+  const lessonList = lessonListData?.data.data || []
+  const extendedLessonList: ExtendedLesson[] = lessonList.map((lesson) => {
+    return { ...lesson, isOpen: false }
+  })
 
   return (
     <Fragment>
@@ -30,6 +45,16 @@ export default function ClassroomDetailForTeacher() {
           )}
 
           {creatingLesson && <ClassroomTeacherCreateLesson setCreatingLesson={setCreatingLesson} />}
+
+          {extendedLessonList.length > 0 && (
+            <div className='space-y-6'>
+              <p className='uppercase text-lg text-center desktop:text-xl font-medium tracking-wide'>Bài giảng</p>
+
+              {extendedLessonList.map((lesson) => (
+                <ClassroomLessonCard key={lesson._id} lesson={lesson} />
+              ))}
+            </div>
+          )}
         </div>
       )}
     </Fragment>
---
+++ src/pages/ClassroomDetail/components/ClassroomLessonCard/ClassroomLessonCard.tsx
@@ -0,0 +1,62 @@
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import classNames from 'classnames'
+import DOMPurify from 'dompurify'
+import { createRef, useRef, useState } from 'react'
+import AnimateChangeInHeight from 'src/components/AnimateChangeInHeight'
+import { Lesson } from 'src/types/lesson.type'
+
+interface Props {
+  lesson: Lesson
+}
+export default function ClassroomLessonCard({ lesson }: Props) {
+  //! Handle content
+  const detailRef = useRef<HTMLDivElement>(null)
+  const itemDescriptionRef = createRef<HTMLDivElement>()
+  const itemDescriptionContentRef = createRef<HTMLDivElement>()
+
+  const [extending, setExtending] = useState<boolean>(false)
+  const extend = () => {
+    setExtending(true)
+  }
+  const collapse = () => {
+    if (detailRef.current) {
+      detailRef.current.scrollIntoView({ behavior: 'smooth' })
+    }
+    setExtending(false)
+  }
+
+  return (
+    <div key={lesson._id} className='p-4 rounded-md bg-webColor100'>
+      <button
+        onClick={extending ? collapse : extend}
+        className='flex hover:text-primaryText text-lg w-full items-center justify-between'
+      >
+        <p className=''>{lesson.name}</p>
+        {extending && <FontAwesomeIcon icon={faChevronUp} />}
+        {!extending && <FontAwesomeIcon icon={faChevronDown} />}
+      </button>
+
+      <AnimateChangeInHeight>
+        <div
+          ref={itemDescriptionRef}
+          className={classNames('', {
+            'max-h-0 overflow-hidden': !extending,
+            'h-full overflow-visible py-1': extending
+          })}
+        >
+          <div
+            ref={itemDescriptionContentRef}
+            dangerouslySetInnerHTML={{
+              __html: DOMPurify.sanitize(lesson.content, {
+                FORCE_BODY: true,
+                ALLOWED_ATTR: ['style', 'classs']
+              })
+            }}
+            className='overflow-visible'
+          />
+        </div>
+      </AnimateChangeInHeight>
+    </div>
+  )
+}
