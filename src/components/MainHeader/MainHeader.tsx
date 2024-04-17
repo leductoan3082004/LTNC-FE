6a2de82fac1ebe606511d8d2d8f4cdda30ffd97d
teddylethal <letienthanh364@gmail.com>
2024-04-17 13:02:24 +0700

    fix main header

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -21,20 +21,20 @@ export default function MainHeader() {
 
   //! Style
   const titleClassname =
-    'text-lightText uppercase justify-center rounded-lg col-span-1 relative flex items-center font-medium desktop:text-lg px-6 hover:bg-hoveringBg'
+    'text-lightText uppercase justify-center rounded-lg col-span-1 relative flex items-center font-medium px-6 hover:bg-hoveringBg'
 
   return (
     <div
       className='w-full bg-webColor700 h-full justify-center items-center text-lightText'
       style={{ minHeight: 'inherit' }}
     >
-      <div className='container flex h-full'>
-        <div className=' w-1/4 flex space-x-2 py-2 justify-start '>
+      <div className='container flex h-full w-full justify-between'>
+        <div className='flex space-x-2 py-2 justify-start '>
           <img className='bg-slate-50 rounded h-12' src='\images\HCMCUT_logo.png' alt='logo' />
           <div className='uppercase text-lg desktop:text-2xl flex items-center font-bold'>LTNC</div>
         </div>
 
-        <div className='w-1/2 flex items-center justify-center'>
+        <div className='flex items-center justify-center'>
           <div className='grid grid-cols-4 shrink-0 h-[70%] gap-2'>
             {menus.map((menu, index) => (
               <NavLink
@@ -104,7 +104,7 @@ export default function MainHeader() {
           </div>
         </div>
 
-        <div className='w-1/4 flex justify-end items-center '>
+        <div className='flex justify-end items-center '>
           {!isAuthenticated && (
             <NavLink
               to={mainPath.login}
